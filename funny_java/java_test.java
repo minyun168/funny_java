b707907b8563b61d9fe3f42b77c7dff40732a53b
minyun168 <minyun168@outlook.com>
2018-11-11 23:59:32 +0800

    Update funny_java/java_test.java

---
+++ funny_java/java_test.java
@@ -0,0 +1,35 @@
+#java test_helloworld
+
+public class Hello
+{
+	public static void main(String args[])
+	{
+	System.out.println("hello world");
+	}
+}
+
+
+#java test
+
+public class java_print
+{
+	public static void main(string args[])
+	{
+		int num;
+		num=3;
+		System.out.println("This is a number"+num)
+		System.out.println("I have "+num+"books");
+		
+	}
+}
+
+
+#java test2
+
+public class java_test2
+{
+	public static void main(string args[])
+	{
+		
+	}
+}
