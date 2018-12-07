69ce5ec0aba5e31d9a3a9d1647d4d5016e47cdd8
minyun168 <minyun168@outlook.com>
2018-12-07 15:12:49 +0800

    Update java_test.java

--- funny_java/java_test.java
+++ funny_java/java_test.java
@@ -13,7 +13,7 @@ public class Hello
 
 public class java_print
 {
-	public static void main(string args[])
+	public static void main(String args[])
 	{
 		int num;
 		num=3;
@@ -31,5 +31,19 @@ public class java_test2
 	public static void main(string args[])
 	{
 		
+	}
+}
+
+#java test3
+
+public class java_test3
+{
+	public static void main(string args[])
+	{
+		int sum = 0;
+		for (int i=1, i<=5;i++)
+			sum = sum +i ;
+			System.out.println("i="+i+"sum="+sum); //The first letter of string and system should be in upper case
+
 	}
 }
